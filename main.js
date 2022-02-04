8001ad628256e06103f37572aa3a161ed40b1383
brackeys21 <78796487+brackeys21@users.noreply.github.com>
2022-02-04 16:20:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var img = "";
+
+function preload()
+{
+    img = loadImg('dog_cat.jpg');
+}
+
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
